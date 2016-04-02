6813bcdd6ced654246d6e97ab49d2cd86f81a7c2
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-02 17:08:22 +0200

    usage info and fixes get_next and get multi

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 simple_snmp
 =====
 
-Simple wrapper around Erlangs builtin SNMP. The goal is to provide easier access to SNMP functionality such as get and get_next.
+Simple wrapper around Erlang's builtin SNMP manager. The goal is to provide easier access to SNMP functionality such as get and get_next.
 
 The initial code is based on the implementation [https://github.com/ates/snmpcl](https://github.com/ates/snmpcl)
 
@@ -9,3 +9,25 @@ Build
 -----
 
     $ rebar3 compile
+
+
+## Usage
+Get:
+
+    1> application:start(simple_snmp).
+    ok
+    2> simple_snmp:get("195.218.195.228", [[1,3,6,1,2,1,1,6,0]]).
+    [{[1,3,6,1,2,1,1,6,0],'OCTET STRING',"Moscow, Russia"}]
+
+Get with multiple OIDs:
+
+    1> simple_snmp:get("195.218.195.228", [[1,3,6,1,2,1,1,5,0],[1,3,6,1,2,1,1,7,0]]).
+    [{[1,3,6,1,2,1,1,5,0],'OCTET STRING',"zeus.snmplabs.com"},
+     {[1,3,6,1,2,1,1,7,0],'INTEGER',72}]
+
+Get next:
+
+    1> application:start(simple_snmp).
+    ok
+    2> simple_snmp:get_next("195.218.195.228", [[1,3,6,1,2,1,1,7,0]]).
+    [{[1,3,6,1,2,1,1,8,0],'TimeTicks',212615850}]
--- src/simple_snmp.erl
+++ src/simple_snmp.erl
@@ -10,7 +10,9 @@
 % API
 -export([
     get/2,
-    get/3
+    get/3,
+    get_next/2,
+    get_next/3
 ]).
 
 start(normal, _Args) ->
@@ -20,23 +22,6 @@ start(normal, _Args) ->
 
 stop(_State) ->
     ok.
-
-get(Address, Oid) ->
-    get(v2, Address, "public", Oid).
-
-get(Address, Community, Oid) ->
-    get(v2, Address, Community, Oid).
-
-get(Version, Address, Community, Oid) ->
-    ok = prepare(Version, Address, Community),
-
-    case snmpm:sync_get(simple_snmp_user, "simple_snmp_user", [Oid]) of
-        {ok, {_, _, Result}, _Remaining} ->
-            [{_, Oid, Type, Value, _}] = Result,
-            {Oid, Type, Value};
-        {error, Reason} ->
-            error_logger:error_msg("Derp ~p ~n", [Reason])
-    end.
 
 prepare(Version, Address, Community) ->
     Options = [
@@ -61,3 +46,41 @@ prepare(Version, Address, Community) ->
             snmpm:unregister_agent(simple_snmp_user, "simple_snmp_user")
     end,
     snmpm:register_agent(simple_snmp_user, "simple_snmp_user", Options).
+
+format_result([], State) ->
+    lists:reverse(State);
+format_result([{_, Oid, Type, Value, _}|T], State) ->
+    format_result(T, [{Oid, Type, Value}|State]).
+
+get(Address, Oids) ->
+    get(v2, Address, "public", Oids).
+
+get(Address, Community, Oids) ->
+    get(v2, Address, Community, Oids).
+
+get(Version, Address, Community, Oids) ->
+    ok = prepare(Version, Address, Community),
+
+    case snmpm:sync_get(simple_snmp_user, "simple_snmp_user", Oids) of
+        {ok, {_, _, Result}, _Remaining} ->
+            format_result(Result, []);
+        {error, Reason} ->
+            error_logger:error_msg("Derp ~p ~n", [Reason])
+    end.
+
+get_next(Address, Oids) ->
+    get_next(v2, Address, "public", Oids).
+
+get_next(Address, Community, Oids) ->
+    get_next(v2, Address, Community, Oids).
+
+get_next(Version, Address, Community, Oids) ->
+    ok = prepare(Version, Address, Community),
+
+    case snmpm:sync_get_next(simple_snmp_user, "simple_snmp_user", [Oids]) of
+        {ok, {_, _, Result}, _Remaining} ->
+            format_result(Result, []);
+        {error, Reason} ->
+            error_logger:error_msg("Derp ~p ~n", [Reason]),
+            {error, Reason}
+    end.
